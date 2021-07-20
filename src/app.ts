4433a12a2c01c2d2e58f71ce9a68bc3718f6e5ab
Thomas McErlean <tjmcerlean@googlemail.com>
2021-07-20 11:57:12 +0100

    set up connection to MongoDB database

--- src/app.ts
+++ src/app.ts
@@ -98,6 +98,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add router to request handling chain
 app.use('/api', apiRouter);
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.env.DB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
