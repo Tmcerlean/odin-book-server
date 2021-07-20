b1d2cf8912ff9f6a6009eab766aa33ed388831cb
Thomas McErlean <tjmcerlean@googlemail.com>
2021-07-20 23:09:51 +0100

    add missing routes for user

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -56,14 +56,20 @@ router.delete('/posts/:id', [
 
 // USER ROUTES
 
+// GET all users - /api/
+router.get('/', userController.user_all_get);
+
 // GET user profile - /api/:userId
 router.get('/:userId', userController.user_get);
 
 // PUT edit user profile - /api/:userId
 router.put('/:userId', userController.user_edit_profile);
 
-// PUT edit user profile image - /api/:userId/profileimage
-router.put('/:userId/profileimage', userController.user_edit_profile_image)
+// DELETE user profile /api/:userId
+router.delete('/:userId', userController.user_delete);
+
+// POST add user profile image - /api/:userId/profileimage
+router.post('/:userId/profileimage', userController.user_add_profile_image)
 
 // POST request for user log in - /api/login
 router.post('/login', userController.login_post);
@@ -73,5 +79,17 @@ router.post('/logout', userController.logout_post);
 
 // POST request for user sign up - /api/signup
 router.post('/signup', userController.signup_post);
+
+// POST send friend request - /api/request
+router.post('/request', userController.friend_request_post);
+
+// DELETE cancel friend request - /api/request
+router.delete('/request', userController.friend_request_delete);
+
+// PUT accept friend request - /api/accept
+router.put('/accept', userController.friend_request_accept);
+
+// DELETE decline friend request - /api/decline
+router.delete('/decline', userController.friend_request_decline);
 
 module.exports = router;
